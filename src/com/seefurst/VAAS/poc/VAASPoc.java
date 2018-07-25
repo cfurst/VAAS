5452a7ad8c6ace64b4c9520d00830e1b5f2874fd
Carl Furst <carl.furst@seefurst.com>
2018-07-25 12:57:48 -0400

    did some java 8 streams and some labeling

--- src/com/seefurst/VAAS/poc/VAASPoc.java
+++ src/com/seefurst/VAAS/poc/VAASPoc.java
@@ -9,6 +9,9 @@ import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
+
+import java.util.Arrays;
+import java.util.List;
 import javax.jcr.LoginException;
 import javax.jcr.Node;
 import javax.jcr.version.VersionHistory;
@@ -47,6 +50,7 @@ public class VAASPoc {
     	System.out.println("is stored at path: " + newContent.getPath());
     	
     	//add versioning
+    	System.out.println("simple: " + Repository.OPTION_SIMPLE_VERSIONING_SUPPORTED); System.out.println("Full: " + Repository.OPTION_VERSIONING_SUPPORTED);
     	newContent.addMixin("mix:versionable");
     	sess.save();
     	//set a version
@@ -60,6 +64,7 @@ public class VAASPoc {
     	sess.save();
     	//System.out.print("done...");
     	//list versions.
+    	//retreive a version
     	System.out.println("retreiving version.");
     	VersionHistory vh = vm.getVersionHistory(newContent.getPath());
     	VersionIterator vi = vh.getAllVersions();
@@ -68,15 +73,40 @@ public class VAASPoc {
     		Version v = vi.nextVersion();
     		System.out.println("Found Version.." + v.getName() );
     		try {
-    			System.out.println("content at version.. " + v.getProperty("content").getString());
+    			//System.out.println("content at version.. " + v.getProperty("content").getString());
+    			Node frozenNode = v.getFrozenNode();
+    			System.out.println("content at version.. " + frozenNode.getProperty("content").getString());
     		} catch (PathNotFoundException a) {
     			System.out.println("no content found...");
     			
     		}
     		
     	}
+    	Version v = vh.getVersion("1.0");
+    	Node frozenNode = v.getFrozenNode();
+    	System.out.println("Got version 1.0 : " + frozenNode.getProperty("content").getString() );
     	
-    	//retreive a version
+    	//change add version and list different content..
+    	newContent.setProperty("content", "The Quick blue fox jumped over the lazy dog");
+    	sess.save();
+    	v = vm.checkpoint(newContent.getPath());
+    	System.out.println("got new vesion: " + v.getName());
+    	frozenNode = v.getFrozenNode();
+    	
+    	System.out.println("new version has text: " + frozenNode.getProperty("content").getString());
+    	
+    	//label
+    	vh.addVersionLabel("1.1","QA",false);
+    	vh.addVersionLabel("1.1", "BETA", false);
+    	String[] labels = vh.getVersionLabels(v);
+    	List<String> labelList = Arrays.asList(labels);
+    	labelList.stream().forEach((lb) -> System.out.println("Got label: " +  lb));
+    	
+    	
+    	
+    	
+    	
+    	
     	
 
     	
